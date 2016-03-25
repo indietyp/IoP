4179e42215b5553b1c0e8c128ab4358557b6374a
Bilal Mahmoud <Bilal-23@gmx.de>
2016-03-25 18:53:57 +0000

    minor fixes

--- testcase.py
+++ testcase.py
@@ -7,17 +7,18 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.pot
-getData(light)
 
 def getData(sensor):
   # plant = db.Plant.find_one({'localhost': True})['abbreviation']
 
-  meshPlants = db.Plant.find({"localhost": {"$ne": True}})
+  meshPlants = db.Plant.find({"localhost": {'$ne': True}})
   if meshPlants != None:
     for plant in meshPlants:
-      lastentry = db.Plant.find_one({'s': sensor[0:1], 'p': plant['abbreviation']}, sort=[("_id", pymongo.DESCENDING)])
-      timestamp = lastentry['_id'].getTimestamp()
-      with urllib.request.urlopen('http://' + meshPlant['ip']+ ':2211/sensor/' + sensor + '/' + timestamp) as response:
+      lastentry = db.SensorData.find_one({'s': sensor[0:1], 'p': plant['abbreviation']}, sort=[("_id", pymongo.DESCENDING)])
+      timestamp = round(lastentry['_id'].generation_time.timestamp())
+      timestamp += 1
+      print(timestamp)
+      with urllib.request.urlopen('http://' + plant['ip']+ ':2211/sensor/' + sensor[0:1] + '/' + str(timestamp)) as response:
          html = response.read()
          print(html)
 
@@ -41,3 +42,4 @@ def getData(sensor):
   #   }
   # )
 
+getData('light')
