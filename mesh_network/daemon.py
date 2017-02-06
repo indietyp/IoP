e65e4085d5709567fccfdf1b9cc901523a3ab77d
indietyp <bilalmahmoud@posteo.de>
2017-02-06 22:20:17 +0100

    public -> private

--- mesh_network/daemon.py
+++ mesh_network/daemon.py
@@ -876,6 +876,10 @@ class MeshNetwork(object):
 
         self.send(80107, plant=local, recipient=target, messages=port)
 
+        private = information['key'][str(local.uuid)]['private']
+        private = MeshTools().hex2bin(private.encode())
+        crypter = RSA.importKey(private)
+
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         host = '0.0.0.0'
         client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
