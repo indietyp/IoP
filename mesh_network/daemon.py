cd077d2f0119411e388049b0a5f8a22c68dae7c8
indietyp <bilalmahmoud@posteo.de>
2017-02-07 22:03:35 +0100

    typecheck

--- mesh_network/daemon.py
+++ mesh_network/daemon.py
@@ -891,6 +891,9 @@ class MeshNetwork(object):
             received = list(received)
             received[0] = toolchain.hex2bin(received[0])
             received[0] = crypter.decrypt(received[0])
+            print(type(received[0]))
+            received[0] = received[0].decode().encode()
+            print(type(received[0]))
 
             self.daemon_process(received)
           except Exception as e:
@@ -932,6 +935,9 @@ class MeshNetwork(object):
             received = list(received)
             received[0] = toolchain.hex2bin(received[0])
             received[0] = crypter.decrypt(received[0])
+            print(type(received[0]))
+            received[0] = received[0].decode().encode()
+            print(type(received[0]))
 
             self.daemon_process(received)
           except Exception as e:
