7a5a3672303b23e4cc170fd3dcf9d531b4e11af2
indietyp <bilalmahmoud@posteo.de>
2017-02-17 15:30:57 +0100

    chunge

--- testing/force_host.py
+++ testing/force_host.py
@@ -1,3 +1,9 @@
 from models.plant import Plant
+for plant in list(Plant.select().where(Plant.host == True)):
+  plant.host = False
+  plant.save()
+
+
 target = Plant.get(name='marta')
+target.host = True
 target.save()
