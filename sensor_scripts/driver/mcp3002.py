96b71a9f1c22fd318c2b4154a1349739cb8d99bf
indietyp <bilalmahmoud@posteo.de>
2017-03-04 18:16:15 +0100

    lib change

--- sensor_scripts/driver/mcp3002.py
+++ sensor_scripts/driver/mcp3002.py
@@ -2,7 +2,7 @@ import spidev
 
 
 class mcp3002:
-  def readadc(device=0, channel=0):
+  def readadc(self, device=0, channel=0):
     if device not in [1, 0] or channel not in [1, 0]:
       return -1
 
@@ -20,17 +20,17 @@ class mcp3002:
 
     return value
 
-  def read_pct(self, adcnum):
-    r = self.readadc(adcnum)
+  def read_pct(self, device=0, channel=0):
+    r = self.readadc(device, channel)
     return int(round((r / 1023.0) * 100))
 
-  def read_3v3(self, adcnum):
-    r = self.readadc(adcnum)
+  def read_3v3(self, device=0, channel=0):
+    r = self.readadc(device, channel)
     v = (r / 1023.0) * 3.3
     return v
 
-  def readadc_avg(self, adcnum):
+  def readadc_avg(self, device=0, channel=0):
     r = []
     for i in range(0, 10):
-      r.append(self.readadc(adcnum))
+      r.append(self.readadc(device, channel))
     return sum(r) / 10.0
