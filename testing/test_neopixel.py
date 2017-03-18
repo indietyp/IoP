fb2047f1d9d02fdd4650ba1600024a40e8959932
indietyp <bilalmahmoud@posteo.de>
2017-03-18 21:34:03 +0100

    linear parralel

--- testing/test_neopixel.py
+++ testing/test_neopixel.py
@@ -43,34 +43,33 @@ def exc():
     bcurrent.append(True if current[pointer] > changelog[pointer] else False)
     bchange.append(True if current[pointer] != changelog[pointer] else False)
 
-  pointers = []
-  for pointer in range(len(bchange)):
-    if bchange[pointer]:
-      pointers.append(pointer)
+  # pointers = []
+  # for pointer in range(len(bchange)):
+  #   if bchange[pointer]:
+  #     pointers.append(pointer)
 
-  print(pointers)
+  # print(pointers)
   old = deepcopy(current)
-  for pointer in pointers:
-    for i in range(0, int(steps / len(pointers) + 1)):
-      color = []
-      for external in range(len(bchange)):
-        if pointer == external:
-          if not bcurrent[pointer]:
-            x = i
-            offset = current[pointer]
-          else:
-            x = int(steps / len(pointers)) - i
-            offset = changelog[pointer]
-          color.append(offset + int(abs(current[external] - changelog[external]) / int(steps / len(pointers)) * x))
-          # color.append(offset + int(math.cos((1 / int(steps / len(pointers))) * math.pi * x) * (abs(current[external] - changelog[external]) / 2) + (abs(current[external] - changelog[external]) / 2)))
+  for i in range(0, steps + 1):
+    color = []
+    for pointer in range(len(bchange)):
+      if bchange[pointer]:
+        if not bcurrent[pointer]:
+          x = i
+          offset = current[pointer]
         else:
-          color.append(old[external])
-      print(color)
-      neopixel.setPixelColorRGB(0, color[0], color[1], color[2])
-      neopixel.show()
-      old = deepcopy(color)
-      time.sleep(0.1)
-      print(color)
+          x = steps - i
+          offset = changelog[pointer]
+        color.append(offset + int(abs(current[pointer] - changelog[pointer]) / steps * x))
+        # color.append(offset + int(math.cos((1 / steps) * math.pi * x) * (abs(current[pointer] - changelog[pointer]) / 2) + (abs(current[pointer] - changelog[pointer]) / 2)))
+      else:
+        color.append(old[pointer])
+    print(color)
+    neopixel.setPixelColorRGB(0, color[0], color[1], color[2])
+    neopixel.show()
+    old = deepcopy(color)
+    time.sleep(0.1)
+    print(color)
 
   with open(basedir + '/ledstate.json', 'w') as out:
     out.write(json.dumps(old))
