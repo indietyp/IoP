084fa5da364bf45f69249b7a064d3414cf0d1136
indietyp <bilalmahmoud@posteo.de>
2017-03-18 20:59:56 +0100

    test changes

--- testing/test_neopixel.py
+++ testing/test_neopixel.py
@@ -10,7 +10,7 @@ def exc():
   neopixel = Adafruit_NeoPixel(1, 18)
   neopixel.begin()
 
-  setting = {'threat': 0, 'cautioning': 0, 'optimum': 1}
+  setting = {'threat': 1, 'cautioning': 0, 'optimum': 0}
   changelog = [0, 0, 0]
 
   if setting['threat'] > 0:
@@ -32,7 +32,7 @@ def exc():
       calc = divmod(raw, 256)
       raw = calc[0]
       current.append(calc[1])
- 
+
     current = current[::-1]
   print(current)
   bcurrent = []
@@ -43,25 +43,30 @@ def exc():
     bcurrent.append(True if current[pointer] >= changelog[pointer] else False)
     bchange.append(True if current[pointer] != changelog[pointer] else False)
 
-  for i in range(0, steps + 1):
-    color = []
-    for pointer in range(len(bchange)):
-      if bchange[pointer]:
-        if not bcurrent[pointer]:
-          x = steps - i
-          # y = changelog[pointer]
-          offset = current[pointer]
+  pointers = []
+  for pointer in range(len(bchange)):
+    if bchange[pointer]:
+      pointers.append(pointer)
+
+  for pointer in pointers:
+    for i in range(0, steps / len(pointers) + 1):
+      color = []
+      for external in range(len(bchange)):
+        if pointer == external:
+          if not bcurrent[pointer]:
+            x = steps - i
+            offset = current[pointer]
+          else:
+            x = i
+            offset = changelog[pointer]
+          color.append(offset + int(math.cos((1 / steps) * math.pi * x) * (abs(current[pointer] - changelog[pointer]) / 2) + (abs(current[pointer] - changelog[pointer]) / 2)))
         else:
-          x = i
-          # y = current[pointer]
-          offset = changelog[pointer]
-        color.append(offset + int(math.cos((1 / steps) * math.pi * x) * (abs(current[pointer] - changelog[pointer]) / 2) + (abs(current[pointer] - changelog[pointer]) / 2)))
-      else:
-        color.append(current[pointer])
-    neopixel.setPixelColorRGB(0, color[0], color[1], color[2])
-    neopixel.show()
-    time.sleep(1 / 30)
-    print(color)
+          color.append(current[pointer])
+      print(color)
+      neopixel.setPixelColorRGB(0, color[0], color[1], color[2])
+      neopixel.show()
+      time.sleep(1 / 15)
+      print(color)
 
   with open(basedir + '/ledstate.json', 'w') as out:
     out.write(json.dumps(color))
